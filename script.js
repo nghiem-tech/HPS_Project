1313e2dc7ce935edec85c5e9dcf360960d2c3728
nghiem-tech <nghinghiem04@gmail.com>
2022-01-28 09:06:55 +1100

    task 5

--- script.js
+++ script.js
@@ -61,6 +61,70 @@ function dragDrop() {
   console.log("dropped");
 }
 
+// Task 5: Date Display 
+
+function printTime() {
+  let d = new Date();
+  let hours = d.getHours();
+  let mins = d.getMinutes();
+  let secs = d.getSeconds();
+  let day = d.getDay();
+  let date = d.getDate();
+  let month = d.getMonth();
+  let year = d.getFullYear();
+  
+  const monthName = [
+  'January','February','March','April',
+  'May','June','July','August','September',
+  'October','November','December'
+];
+
+      switch (day){
+      
+          case 0:
+              day = "Sunday";
+              break;
+          case 1:
+              day = "Monday";
+              break;
+          case 2:
+              day = "Tuesday";
+              break;
+          case 3:
+              day = "Wednesday";
+              break;
+          case 4:
+              day = "Thursday";
+              break;
+          case 5:
+              day = "Friday";
+              break;
+          case 6:
+              day = "Saturday";
+              break;
+         }
+  
+  
+      if(hours>12) {
+          hours = hours - 12;
+      } else {
+          hours = hours
+      }
+      if(mins<10){
+          
+          mins = "0" + mins;
+      }
+      if(secs<10){
+
+          secs = "0" + secs;
+      }
+ 
+month = month + 1; document.getElementById("time-display").innerHTML = hours + ":" + mins + ":" + secs;
+document.getElementById("date-display").innerHTML = day + ", " + date + "  " + monthName[month] + "  " + year;
+}
+setTimeout(printTime, 1);
+setInterval(printTime, 1000);
+
 // Delete task
 
 const delete_btns = document.querySelectorAll(".delete");
