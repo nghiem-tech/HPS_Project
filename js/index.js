a29e2a7e7a37d35b4bad963c83669133fe4f590f
Hon Sang Kong <hk1628@hotmail.com>
2022-02-06 12:12:12 +1100

    test version

--- js/index.js
+++ js/index.js
@@ -203,3 +203,43 @@ newTaskForm.addEventListener("submit", (event) => {
     taskManager.render();
   }
 });
+
+const taskList = document.querySelector("#task-list");
+// Add an 'onclick' event listener to the Tasks List
+taskList.addEventListener("click", (event) => {
+  // Check if a "Done" button was clicked
+  if (event.target.classList.contains("done-button")) {
+    // Get the correct parent Task, yours might be slightly different
+    // Use console.log(event.target.parentElement) to see
+    const parentTask =
+      event.target.parentElement.parentElement.parentElement.parentElement;
+    // Get the taskId of the parent Task and turn it into a number.
+    const taskId = Number(parentTask.dataset.taskId);
+    // Get the task from the TaskManager using the taskId
+    const task = taskManager.getTaskById(taskId);
+    // Update the task status to 'DONE'
+    task.status = "Done";
+    taskManager.save();
+    // Render the tasks
+    taskManager.render();
+  }
+
+  // Check if a "Delete" button was clicked
+  if (event.target.classList.contains("delete-button")) {
+    // Get the parent Task
+    const parentTask =
+      event.target.parentElement.parentElement.parentElement.parentElement;
+
+    // Get the taskId of the parent Task.
+    const taskId = Number(parentTask.dataset.taskId);
+
+    // Delete the task
+    taskManager.deleteTask(taskId);
+
+    // Save the tasks to localStorage
+    taskManager.save();
+
+    // Render the tasks
+    taskManager.render();
+  }
+});
--- js/taskManager.js
+++ js/taskManager.js
@@ -98,7 +98,63 @@ render() {
     reviewTasksList.innerHTML = reviewTaskHtml;
     doneTasksList.innerHTML = doneTaskHtml;
   }
+  
+  save() {
+    // Create a JSON string of the tasks
+    const tasksJson = JSON.stringify(this.tasks);
+
+    // Store the JSON string in localStorage
+    localStorage.setItem("tasks", tasksJson);
+
+    // Convert the currentId to a string;
+    const currentId = String(this.currentId);
+
+    // Store the currentId in localStorage
+    localStorage.setItem("currentId", currentId);
+  }
+
+  load() {
+    // Check if any tasks are saved in localStorage
+    if (localStorage.getItem("tasks")) {
+      // Get the JSON string of tasks in localStorage
+      const tasksJson = localStorage.getItem("tasks");
+
+      // Convert it to an array and store it in our TaskManager
+      this.tasks = JSON.parse(tasksJson);
+    }
+
+    // Check if the currentId is saved in localStorage
+    if (localStorage.getItem("currentId")) {
+      // Get the currentId string in localStorage
+      const currentId = localStorage.getItem("currentId");
+
+      // Convert the currentId to a number and store it in our TaskManager
+      this.currentId = Number(currentId);
+    }
+  }
+
+  deleteTask(taskId) {
+    // Create an empty array and store it in a new variable, newTasks
+    const newTasks = [];
+
+    // Loop over the tasks
+    for (let i = 0; i < this.tasks.length; i++) {
+      // Get the current task in the loop
+      const task = this.tasks[i];
+
+      // Check if the task id is not the task id passed in as a parameter
+      if (task.id !== taskId) {
+        // Push the task to the newTasks array
+        newTasks.push(task);
+      }
+    }
+
+    // Set this.tasks to newTasks
+    this.tasks = newTasks;
+  }
+
 }
+
 
 // Drag and Drop Functionality
 
